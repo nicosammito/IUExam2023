d82e37daefcc057efcbcee5042de3ab0f25b0d35
noahrdk123 <147412574+noahrdk123@users.noreply.github.com>
2023-11-27 14:27:55 +0100

    verrückte Dinge sind passiert

--- js/index.js
+++ js/index.js
@@ -9,11 +9,12 @@ function openTab(evt, tabName) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
+
+  /* Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+  } */
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
