9bbfe0e12b9e12b9bf8ce6b00b83d63f72fcb256
noahrdk123 <147412574+noahrdk123@users.noreply.github.com>
2023-12-01 13:04:31 +0100

    hab alles in einen ordner gepackt

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function openTab(evt, tabName) {
   }
 
 
-  //Get all elements with class="tablinks" and remove the class "active"
+  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.querySelectorAll('.tablinks, .intab-button, .dropdown-item');
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -20,3 +20,15 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+  // Progress bar Homepage
+  let procom = document.getElementById("progress-company");
+  let promod = document.getElementById("progress-model");
+  let prodef = document.getElementById("progress-defect");
+  let propri = document.getElementById("progress-price");
+
+  
+  
+  const progressc = () => {
+    console.log(procom);
+  }
