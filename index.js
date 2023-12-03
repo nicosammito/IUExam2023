cb4ac9545cfb2f0edd9167c105bfa09e130439e5
nicosammito <nico@sammito.de>
2023-12-03 14:39:08 +0100

    fetch

--- index.js
+++ index.js
@@ -1,45 +1,77 @@
-const companyTab = document.querySelector("#tcompany");
-let tmod = document.getElementById("tmodel");
-let tdef = document.getElementById("tdefect");
+const openTab = (tabID) => {
+
+    const tabContent = document.querySelector(`.tabcontent[attr-tab="${tabID}"]`)
+    const tabContents = document.querySelectorAll(".tabcontent")
+    const tabLink = document.querySelector(`.tablinks#${tabID}`)
+    const tabLinks = document.querySelectorAll(".tablinks")
 
 
-const hidecontent = () => {
-  const tabcontent = document.getElementsByClassName("tabcontent");
-  for (tabcontent of tabcontent) {
-    tabcontent.style.display = "none"
-  }
-}
+    tabContents.forEach((tabContent) => {
+        tabContent.classList.remove("active")
+    })
 
-const openTab = (tab) => {
-   
-   const tabID = tab.id
-   const tabContent = document.querySelectorAll(`.tabcontent[attr-tab="${tabID}"]`)[0]
-   const tabContents = document.querySelectorAll(".tabcontent")
+    tabContent.classList.add("active")
 
-   tabContents.forEach((tabContent) => {
-     tabContent.classList.remove("active")
-   })
 
-   tabContent.classList.add("active")
-   
-   const tabLinks = document.querySelectorAll(".tablinks")
-  
-   tabLinks.forEach((tabLink) => {
-    tabLink.classList.remove("active")
-  })
-   tab.classList.add("active")
+    tabLinks.forEach((tabLink) => {
+        tabLink.classList.remove("active")
+    })
+    tabLink.classList.add("active")
 }
 
 const fetchCompany = () => {
-  fetch("https://iu-backend.onrender.com/manufacturer/", {
-    method: "GET",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-  }).then((response) => {
-    return response.json()
-  }).then((json) => {
-    console.log(json)
-  })
+    fetch("https://iu-backend.onrender.com/manufacturer/", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }).then((response) => {
+        return response.json()
+    }).then((json) => {
+        json.response.manufacturers.forEach(manufacturer => {
+            addCompany(manufacturer)
+        })
+    })
+}
+
+fetchCompany();
+const addCompany = (dataCompany) => {
+
+    const tabContentCompany = document.querySelector('.tabcontent[attr-tab="company"]')    //
+
+    const button = document.createElement("button")
+    button.classList.add("intab-button", "btn")
+
+    button.addEventListener("click", () => {
+        fetchDevices(dataCompany.slug)
+        openTab("devices")
+    })
+
+    const icon = document.createElement("i");
+    icon.classList.add("ti", `ti-${dataCompany.icon}`)
+
+    const name = document.createElement("span")
+    name.innerText = dataCompany.name
+
+    button.appendChild(icon)
+    button.appendChild(name)
+
+    tabContentCompany.appendChild(button)
+
+}
+
+const fetchDevices = (manufacturer) => {
+    fetch(`https://iu-backend.onrender.com/manufacturer/${manufacturer}/devices`, {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }).then((response) => {
+        return response.json()
+    }).then((json) => {
+        console.log(json)
+        //TODO: set device into HTML
+    })
 }
